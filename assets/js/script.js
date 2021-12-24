deb503466f9149fbcf6e2c354ab9e1132841624f
George <76901075+Biadedigital@users.noreply.github.com>
2021-12-24 23:46:50 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,55 @@
 function loader(){
-    document.querySelector('.loader-container').classList.add('fade-out');
-}
-  
+  document.querySelector('.loader-container').classList.add('fade-out');
+};
+
 function fadeOut(){
-    setInterval(loader, 5000);
-}
-  
+  setInterval(loader, 5000);
+};
+
 window.onload = fadeOut;
+
+let searchBtn = document.querySelector('#search-btn');
+let searchForm = document.querySelector('.header .search-form');
+
+searchBtn.onclick = () =>{
+  searchBtn.classList.toggle('fa-times');
+  searchForm.classList.toggle('active');
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+};
+
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+  searchBtn.classList.remove('fa-times');
+  searchForm.classList.remove('active');
+};
+
+window.onscroll = () =>{
+  searchBtn.classList.remove('fa-times');
+  searchForm.classList.remove('active');
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+};
+
+let slides = document.querySelectorAll('.home .slide');
+let index = 0;
+
+function next(){
+  slides[index].classList.remove('active');
+  index = (index + 1) % slides.length;
+  slides[index].classList.add('active');
+};
+
+function prev(){
+  slides[index].classList.remove('active');
+  index = (index - 1 + slides.length) % slides.length;
+  slides[index].classList.add('active');
+};
+
 
 let backtotop = select('.back-to-top')
 if (backtotop) {
@@ -19,50 +62,4 @@ if (backtotop) {
   }
   window.addEventListener('load', toggleBacktotop)
   onscroll(document, toggleBacktotop)
-}
-
-let searchBtn = document.querySelector('#search-btn');
-let searchForm = document.querySelector('.header .search-form');
-
-searchBtn.onclick = () =>{
-    searchBtn.classList.toggle('fa-times');
-    searchForm.classList.toggle('active');
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-}
-
-let menu = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.header .navbar');
-
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-    searchBtn.classList.remove('fa-times');
-    searchForm.classList.remove('active');
-}
-
-window.onscroll = () =>{
-    searchBtn.classList.remove('fa-times');
-    searchForm.classList.remove('active');
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-}
-
-let slides = document.querySelectorAll('.home .slide');
-let index = 0;
-
-function next(){
-    slides[index].classList.remove('active');
-    index = (index + 1) % slides.length;
-    slides[index].classList.add('active');
-}
-
-function prev(){
-    slides[index].classList.remove('active');
-    index = (index - 1 + slides.length) % slides.length;
-    slides[index].classList.add('active');
-}
-
-
-
-
+};
